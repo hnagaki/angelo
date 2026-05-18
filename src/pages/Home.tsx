813d5cca65abe4ee79f7978f6ff7e47aea0b2fd0
n-hikaru <n-hikaru@iij.ad.jp>
2026-05-18 06:51:33 +0000

    リンク追加

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,2 +1,8 @@
-const Home = () => <h2>ホーム画面</h2>;
+const Home = () => {
+  return (
+    <a href="https://tabelog.com/fukuoka/A4004/A400404/40042301/">
+      アンジェロ食べログ
+    </a>
+  );
+};
 export default Home;
