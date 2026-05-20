17b5508aa94ab3e1f42b1fd8bce4bda3b15abf2e
n-hikaru <n-hikaru@iij.ad.jp>
2026-05-20 07:15:12 +0000

    コミット

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
 const Home = () => {
   return (
-    <a href="https://tabelog.com/fukuoka/A4004/A400404/40042301/">
-      アンジェロ食べログ
-    </a>
+    <>
+      <a href="https://tabelog.com/fukuoka/A4004/A400404/40042301/">
+        アンジェロ食べログ
+      </a>
+      <h1>いらっしゃいませ</h1>
+    </>
   );
 };
 export default Home;
